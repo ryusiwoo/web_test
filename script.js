1ba637d98dbf8975fc617040a7a848589998e861
ryusiwoo <colkol0516@gmail.com>
2025-04-15 14:32:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // 🔽 Firebase 설정 코드를 여기에 붙여넣으세요 🔽
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBPHER9uMFWodYGEVsdj2KGY_m8HEOCUAQ",
-    authDomain: "comments-24767.firebaseapp.com",
-    databaseURL: "https://comments-24767-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "comments-24767",
-    storageBucket: "comments-24767.firebasestorage.app",
-    messagingSenderId: "559876960346",
-    appId: "1:559876960346:web:faf067629f4c00497de863",
-    measurementId: "G-T2DTS2SY4D"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBPHER9uMFWodYGEVsdj2KGY_m8HEOCUAQ",
+  authDomain: "comments-24767.firebaseapp.com",
+  databaseURL: "https://comments-24767-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "comments-24767",
+  storageBucket: "comments-24767.firebasestorage.app",
+  messagingSenderId: "559876960346",
+  appId: "1:559876960346:web:faf067629f4c00497de863",
+  measurementId: "G-T2DTS2SY4D"
+};
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
 let allComments = [];     // 전체 댓글 저장용
-let expanded = false;     // 확장 여부
+let expanded = false;     // 확장 여부 (초기값은 false로 설정)
 
 // Firebase에서 댓글을 가져오는 부분
 db.ref('comments').on('value', snapshot => {
@@ -80,31 +80,3 @@ function submitComment() {
   });
   document.getElementById('message').value = '';
 }
-
-db.ref('comments').on('value', snapshot => {
-  const commentsDiv = document.getElementById('comments');
-  commentsDiv.innerHTML = '';
-  const comments = snapshot.val();
-  if (comments) {
-    Object.values(comments)
-      .sort((a, b) => b.timestamp - a.timestamp)
-      .forEach(comment => {
-        const date = new Date(comment.timestamp);
-        const timeString = date.toLocaleString('ko-KR', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit'
-        });
-        
-        const div = document.createElement('div');
-        div.className = 'comment';
-        div.innerHTML = `
-          ${comment.message}<br>
-          <small style="color:gray;">${timeString}</small>
-        `;
-        commentsDiv.appendChild(div);
-      });
-  }
-});
